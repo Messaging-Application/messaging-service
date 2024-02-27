c0685af7a9b7a039a0592d6ae0f0f41c04b14a6e
Afonso Boto <afonsomsboto@gmail.com>
2024-02-27 19:25:28 +0100

    Get Chat Implementation and store message + Tests

--- src/main/java/ma/messaging/messagingservice/service/MessageService.java
+++ src/main/java/ma/messaging/messagingservice/service/MessageService.java
@@ -1,12 +1,14 @@
 package ma.messaging.messagingservice.service;
 
 import ma.messaging.messagingservice.model.Message;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface MessageService {
 
-    boolean getMessagesFromUser(UUID senderId, UUID receiverId);
+    List<Message> getMessagesFromChat(UUID chatId);
+
+    Message storeMessage(Message message);
+
 }
--- src/main/java/ma/messaging/messagingservice/service/MessageServiceImpl.java
+++ src/main/java/ma/messaging/messagingservice/service/MessageServiceImpl.java
@@ -1,13 +1,30 @@
 package ma.messaging.messagingservice.service;
 
+import ma.messaging.messagingservice.model.Message;
+import ma.messaging.messagingservice.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class MessageServiceImpl implements MessageService{
+
+    @Autowired
+    MessageRepository messageRepository;
+
     @Override
-    public boolean getMessagesFromUser(UUID fromUserId, UUID toUserId) {
-        return false;
+    public List<Message> getMessagesFromChat(UUID chatId) {
+        return messageRepository.findByChatId(chatId);
     }
+
+    @Override
+    public Message storeMessage(Message message) {
+
+        return messageRepository.save(message);
+    }
+
+
 }
