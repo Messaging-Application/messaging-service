bd74e1edbd889431f422260ef7151551e98d853d
Afonso Boto <afonsomsboto@gmail.com>
2024-03-16 15:39:12 +0100

    Added Retry to socket timeout exception

--- src/main/java/ma/messaging/messagingservice/MessagingServiceApplication.java
+++ src/main/java/ma/messaging/messagingservice/MessagingServiceApplication.java
@@ -5,8 +5,9 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.retry.annotation.EnableRetry;
 
-
+@EnableRetry
 @SpringBootApplication
 public class MessagingServiceApplication {
     private static final Logger logger = LogManager.getLogger(MessagingServiceApplication.class);
