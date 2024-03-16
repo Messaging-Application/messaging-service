4de4ba870c8e84864c59d540a1d8c35ede58c9b2
Afonso Boto <afonsomsboto@gmail.com>
2024-03-16 15:46:09 +0100

    changed MongoURI

--- src/main/java/ma/messaging/messagingservice/repository/MessageRepository.java
+++ src/main/java/ma/messaging/messagingservice/repository/MessageRepository.java
@@ -1,11 +1,8 @@
 package ma.messaging.messagingservice.repository;
 
-import com.mongodb.MongoSocketReadTimeoutException;
 import ma.messaging.messagingservice.model.Message;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,7 +12,6 @@ import java.util.UUID;
 @Repository
 public interface MessageRepository extends MongoRepository<Message, String> {
 
-    @Retryable(value = MongoSocketReadTimeoutException.class, maxAttempts = 4, backoff = @Backoff(delay = 1000))
     List<Message> findByChatId(UUID chat_id);
 
 }
