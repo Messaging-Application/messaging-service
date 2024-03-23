f3eba223633502047a48bdd15b74479e785bdc65
Afonso Boto <afonsomsboto@gmail.com>
2024-03-24 00:28:46 +0100

    fix tests

--- src/test/java/ma/messaging/messagingservice/MessagingServiceApplicationTests.java
+++ src/test/java/ma/messaging/messagingservice/MessagingServiceApplicationTests.java
@@ -1,10 +1,15 @@
 package ma.messaging.messagingservice;
 
+import com.amazonaws.services.sqs.AmazonSQS;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class MessagingServiceApplicationTests {
+
+    @MockBean
+    private AmazonSQS amazonSQS;
 
     @Test
     void contextLoads() {
